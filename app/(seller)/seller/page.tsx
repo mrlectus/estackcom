6be6c8c94c322d7eddf8c45f40c604f0e176a344
mrlectus <lilbrown3000@gmail.com>
2024-10-15 13:19:48 +0100

    forgot order count

--- app/(seller)/seller/page.tsx
+++ app/(seller)/seller/page.tsx
@@ -3,16 +3,22 @@ import { estackApi } from "@/app/services";
 import { CountCard } from "@/components/count-card";
 
 const SellerDashboard = () => {
-  const { data, isLoading } = estackApi.useGetUserProductQuery();
+  const product = estackApi.useGetUserProductQuery();
+  const order = estackApi.useGetSellerOrderQuery();
   return (
     <main className="flex p-4">
       <div className="md:flex gap-3">
-        <CountCard text1={"Total Order"} text2={"Sale Count"} count={200} />
+        <CountCard
+          text1={"Total Order"}
+          text2={"Sale Count"}
+          loading={order.isLoading}
+          count={order.data?.length ? order.data.length : 0}
+        />
         <CountCard
           text1={"Total Product Count"}
           text2={"product Count"}
-          count={data?.length ? data.length : 0}
-          loading={isLoading}
+          count={product.data?.length ? product.data.length : 0}
+          loading={product.isLoading}
         />
       </div>
     </main>
